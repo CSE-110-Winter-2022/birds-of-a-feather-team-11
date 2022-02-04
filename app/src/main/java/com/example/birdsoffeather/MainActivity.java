864932b1775a2975e576a21c10f4474092c45472
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2022-02-03 21:48:04 -0800

    validate image url before storing. issue #10

--- app/src/main/java/com/example/birdsoffeather/MainActivity.java
+++ app/src/main/java/com/example/birdsoffeather/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.birdsoffeather;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -35,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        //Intent intent = new Intent(this, UploadPhoto.class);
+        //startActivity(intent);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/birdsoffeather/UploadPhoto.java
+++ app/src/main/java/com/example/birdsoffeather/UploadPhoto.java
@@ -3,13 +3,13 @@ package com.example.birdsoffeather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.InputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class UploadPhoto extends AppCompatActivity {
 
@@ -17,18 +17,39 @@ public class UploadPhoto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_photo);
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        if (preferences.getString("Name", null) != null) {
+            //Intent intent = new Intent(this, EnterClasses.class);
+            //startActivity(intent);
+            finish();
+        }
     }
     public void onSkipButtonClick(View view) {
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("Photo URL", "@string/default_photo_url");
-        editor.apply();
+        submitURL("@string/default_photo_url");
     }
     public void onSubmitButtonClick(View view) {
+        TextView photoURLView = findViewById(R.id.photo_url);
+        String photoURL = photoURLView.getText().toString();
+        boolean isValidImage = false;
+        try {
+            URLConnection connection = new URL(photoURL).openConnection();
+            String contentType = connection.getHeaderField("Content-Type");
+            isValidImage = contentType.startsWith("image/");
+        } catch (IOException e) {}
+        if (isValidImage) {
+            submitURL(photoURL);
+        } else {
+            Utilities.showAlert(this, "Please input a valid image URL!");
+        }
+    }
+
+    public void submitURL(String url) {
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        TextView photoURLView = findViewById(R.id.photo_url);
-        editor.putString("Photo URL", photoURLView.getText().toString());
+        editor.putString("Photo URL", url);
         editor.apply();
+        //Intent intent = new Intent(this, EnterClasses.class);
+        //startActivity(intent);
+        finish();
     }
 }
