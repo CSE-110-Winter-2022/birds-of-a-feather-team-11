200f764842ca8ab27801dae7d8a626b79de957d9
Shubham Bhargava <shubhargava@gmail.com>
2022-02-06 12:38:37 -0800

    Completed unit tests

--- app/src/test/java/com/example/birdsoffeather/CreateProfileTest.java
+++ app/src/test/java/com/example/birdsoffeather/CreateProfileTest.java
@@ -2,6 +2,13 @@ package com.example.birdsoffeather;
 
 import static org.junit.Assert.assertEquals;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -26,11 +33,21 @@ public class CreateProfileTest {
     @Test
     public void modifyNameTest() {
 
+        ActivityScenario<CreateProfile> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            EditText nameEditText = activity.findViewById(R.id.name_edit_text);
+            nameEditText.setText("John");
+
+            Button button = activity.findViewById(R.id.confirm_button);
+            button.performClick();
+
+            SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
+            assertEquals("John", preferences.getString("Name", null));
+        });
     }
 
-    @Test
-    public void invalidModifiedNameTest() {
-
-    }
 
 }
