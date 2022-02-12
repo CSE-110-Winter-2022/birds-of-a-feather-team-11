68be3a8816668fa631c19a2230ba856273e350bd
christian <christian@christians-MacBook-Pro.local>
2022-02-12 08:24:21 -0800

    user can skip google sign-in

--- app/src/main/java/com/example/birdsoffeather/GoogleSignInPrompt.java
+++ app/src/main/java/com/example/birdsoffeather/GoogleSignInPrompt.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -103,5 +104,11 @@ public class GoogleSignInPrompt extends AppCompatActivity {
 
             Intent intent = new Intent(this, CreateProfile.class);
         }
+    }
+
+    public void onSkipButtonClicked(View view) {
+        Intent intent = new Intent(this, CreateProfile.class);
+        finish();
+        startActivity(intent);
     }
 }
