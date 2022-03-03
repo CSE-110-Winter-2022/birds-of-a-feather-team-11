49fdfb8702beb7de26ae75b2cf571f395b6adc8d
MeghajV <emailmeghaj@gmail.com>
2022-03-02 17:08:51 -0800

    added sample test

--- app/src/test/java/com/example/birdsoffeather/CreateSessionTest.java
+++ app/src/test/java/com/example/birdsoffeather/CreateSessionTest.java
@@ -1,9 +1,12 @@
 package com.example.birdsoffeather;
 
+import static org.junit.Assert.assertEquals;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.concurrent.ExecutorService;
@@ -15,4 +18,9 @@ public class CreateSessionTest {
 
     @Rule
     public ActivityScenarioRule<EnterClasses> scenarioRule = new ActivityScenarioRule<>(EnterClasses.class);
+
+    @Test
+    public void sampleTest() {
+        assertEquals(2, 1+1);
+    }
 }
